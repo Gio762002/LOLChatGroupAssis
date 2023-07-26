26f92b55e8a56102a296803c40a5e6b0141f3cc9
Gio762002 <guo6437@outlook.com>
2023-07-26 11:32:34 +0200

    add fonction that scrapes multi-kill info

--- opgg_scraper.py
+++ opgg_scraper.py
@@ -5,12 +5,24 @@ from lxml import etree
 import numpy as np
 
 class OpggScraper:
+
     def __init__(self) -> None:
         self.chrome_options = Options()
         self.chrome_options.add_argument('--headless')
         self.chrome_options.add_argument('--disable-gpu')
         self.chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
         self.driver = webdriver.Chrome(options=self.chrome_options)
+
+
+    def check_xpath(self):
+        try_overview = self.get_overview(['Magnoliar'])
+        try_last_twenty_matches = self.get_last_twenty_matches(['Magnoliar'])
+    
+        def has_empty_list(dataframe):
+            return any(x == [] for x in dataframe['Magnoliar'].values())
+        if has_empty_list(try_overview) or has_empty_list(try_last_twenty_matches):
+            return True #not need to adapt the xpath
+
 
     def get_overview(self,players):
         overview = {}
@@ -58,8 +70,22 @@ class OpggScraper:
                 kda = np.array(lst_kda).astype(int).transpose() # 3*20
                 last_twenty_matches[player] = {"kda":kda}
             
-            # get multi_kill array
-            
+                # get multi_kill list
+                mk_map = {
+                    0: 0,
+                    'Double Kill': 2,
+                    'Triple Kill': 3,
+                    'Quadra Kill': 4,
+                    'Penta Kill': 5
+                }
+
+                lst_mk=[]
+                for i in range(1,21):
+                    mk = dom.xpath(f'//*[@id="content-container"]/div[2]/div[3]/li[{i}]/div/div[1]/div[1]/div[@class="info"]/div[2]/div[@class="multi-kill"]/text()')
+                    lst_mk.append(mk[0] if mk != [] else 0)
+                multi_kill = [mk_map[i] for i in lst_mk]
+                last_twenty_matches[player]["multi_kill"] = multi_kill
+                
 
             except Exception as e:
                 print(f"Error: {e}")
@@ -71,6 +97,6 @@ class OpggScraper:
 
 
 OpS = OpggScraper()
-players = ['Magnoliar','ˆĘKKØˆ']
+players = ['Magnoliar']
 print(OpS.get_last_twenty_matches(players))
 OpS.close()
